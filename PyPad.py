05af8730bcdd58bf4f959fea43dd69f678ffd96d
Thims <root.packet@gmail.com>
2023-08-21 19:24:40 -0400

    Working introduction of OOP style. GUI interface runs, but holds ZERO functional manipulation.

--- PyPad.py
+++ PyPad.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python3
-
-
-
-# PyPad - Python Organized ScratchPad for scientific purposes
-#
-# v0.0.1 Beta
-# Date: 8-19-2023
-# Author: Tom Smith (Thomas DOT Briggs DOT Smith AT Gmail DOT com)
-
-
-
 import os
 from os import path
 from tkinter import *
@@ -18,217 +6,125 @@ from tkinter import messagebox
 
 
 
-# Environment variables
-user_data = "userData"
-categ_path = "./" + user_data + "/Categories/"
 
-slctn_path = {'ctgry' : '', 'file' : ''}
+class PyPadGUI():
+    categ_path = "./userData/Categories/"
 
 
-# Operation Variables
-
-
-
-'''
-Global Function Delcarations
-'''
-def add_categ():
-    ctg_win = Toplevel(root)
-    ctg_win.geometry("200x200")
-    ctg_win.title("Child Window")
+    def __init__(self):
+        self.__GUI__()
+        self.__file_menu__()
+        self.__ui_frames__()
+        self.__categories__()
+        self.__files__()
+        self.__notepad__()
     
-    lbl = Label(ctg_win, text="Label:")
-    lbl.pack(side="left")
     
-    add_categ_var = ""
-    entry = Entry(ctg_win, textvariable=add_categ_var)
-    entry.pack(side="right")
+    def __GUI__(self):
+        self.root = Tk()
+        self.root.geometry("777x575")
+        self.root.minsize(777, 575)
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=200)
+
+        self.root.rowconfigure(0, weight=1)
     
-    btn = Button(ctg_win, text="Ok", command=ctg_win.destroy)
-    btn.pack(side="bottom")
+    
+    def __file_menu__(self):
+        ### File Bar
+        self.menubar = Menu(self.root)
+
+        # File
+        self.filemenu = Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Open", command="")
+        self.filemenu.add_command(label="Save", command="") #save_notepad)
+        self.filemenu.add_command(label="Close", command="")
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Exit", command=self.root.quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
 
 
-# Populate a list of files from userData/Categories/AAA
-def get_files(Category):
-    file_list = []
+        # Help
+        self.helpmenu = Menu(self.menubar, tearoff=0)
+        self.helpmenu.add_command(label="User Manual", command="")
+        self.helpmenu.add_command(label="Website", command="")
+        self.helpmenu.add_separator()
+        self.helpmenu.add_command(label="About", command="")
+        self.menubar.add_cascade(label="Help", menu=self.helpmenu)
+        
+        # Stack our config
+        self.root.config(menu=self.menubar)
     
-    for file in os.listdir("./userData/Categories/" + Category):
-        file_list.append(file)
+    
+    def __ui_frames__(self):
+        self.left_frame = Frame(self.root)
+        self.left_frame.pack(side="left", fill="y")
+
+        self.right_frame = Frame(self.root)
+        self.right_frame.pack(side="right", fill="both", expand=1)
+    
+    
+    def __categories__(self):
+        # Category Buttons
+        self.cat_btn_frame = Frame(self.left_frame)
+        self.cat_btn_frame.pack(side="top")
+
+        self.cat_add = Button(self.cat_btn_frame, text="Add", command="") #add_categ)
+        self.cat_add.pack(side="left")
+
+        self.cat_edit = Button(self.cat_btn_frame, text="Edit", command="")
+        self.cat_edit.pack(side="left")
+
+        self.cat_del = Button(self.cat_btn_frame, text="Delete", command="")
+        self.cat_del.pack(side="left")
+
+
+        # Categories Listview
+        self.cat_list_frame = Frame(self.left_frame)
+        self.cat_list_frame.pack(side="top")
+
+        self.cat_scrllbr = Scrollbar(self.cat_list_frame)
+        self.cat_scrllbr.pack(side=RIGHT, fill=Y)
+
+        self.cat_list = Listbox(self.cat_list_frame)#, yscrollcommand=cat_scrllbr.set)
+        
+        # Populate categories by directory structure
+        if path.isdir(self.categ_path):
+            categories = os.listdir(self.categ_path)
+            categories.sort()
+        else:
+            print("ERROR: ", self.categ_path, ": does not exist.")
+            os.exit(1)
+        n = 0
+        for categ in categories:
+            self.cat_list.insert(n, categ)
+            self.cat_list.bind('<Double-1>', "") #categ_dc_event)
+            n += 1
             
-    return file_list
+        self.cat_list.pack(side="top")
 
 
-# Open our text document and write contents to Text()
-def change_notepad(event):
-    indx = file_list.curselection()[0]
-    read_file = file_list.get(indx)
-    slctn_path['file'] = read_file
+    def __files__(self):
+        # File listview
+        self.file_list_frame = Frame(self.left_frame)
+        self.file_list_frame.pack(side="top")
+
+        self.file_scrllbr = Scrollbar(self.file_list_frame)
+        self.file_scrllbr.pack(side=RIGHT, fill=Y)
+
+        self.file_list = Listbox(self.file_list_frame)#, yscrollcommand=self.file_scrllbr.set)
+        self.file_list.insert(0, "species.txt")
+        self.file_list.insert(1, "recipes.txt")
+        self.file_list.insert(2, "procedures.txt")
+        self.file_list.insert(3, "notes.txt")
+        self.file_list.pack(side="top")
+
+
+    def __notepad__(self):
+        # Notepad Window
+        self.notepad = Text(self.right_frame)
+        self.notepad.pack(side="right", fill="both", expand=1)
+
     
-    try:
-        fd = os.open("./userData/Categories/" + slctn_path['ctgry'] + "/" + slctn_path['file'], os.O_RDONLY)
-    except FileNotFoundError:
-        print("ERROR: that file does not exist. TODO: Make this a messageBOX")
-    
-    notepad.delete("0.0", END)
-    
-    chunk = os.read(fd, 1024)
-    while chunk != b'':
-        notepad.insert(END, chunk)
-        chunk = os.read(fd, 1024)
-    
-    os.close(fd)
-
-
-# Category list double click event
-def categ_dc_event(event):
-    clear_notepad()
-    
-    lbox = cat_list.curselection()[0]
-    selctn = cat_list.get(lbox)
-    slctn_path['ctgry'] = selctn
-    
-    # Clear the filename listbox
-    file_list.delete(0, END)
-    
-    # Get our files from userData
-    files = get_files(selctn)
-    
-    # Update our file listbox
-    
-    for file in files:
-        file_list.insert(END, file)
-        file_list.bind('<Double-1>', change_notepad)
-
-
-
-
-# Clear notepad, saving first
-def clear_notepad():
-    save_notepad()
-    notepad.delete("0.0", END)
-
-
-# Save contents of notepad
-def save_notepad():
-    try:
-        fd = os.open("./userData/Categories/" + slctn_path['ctgry'] + "/" \
-            + slctn_path['file'], os.O_WRONLY|os.O_CREAT)
-    except FileNotFoundError:
-        print("ERROR: A weird one.")
-        
-    # TODO: Make this section write through chunks from notepad
-    blob = notepad.get("0.0", END).encode()
-    os.write(fd, blob)
-    
-    os.close(fd)
-
-
-
-
-# Get the present category directories
-if path.isdir(categ_path):
-    categories = os.listdir(categ_path)
-    categories.sort()
-else:
-    print("ERROR: ", categ_path, ": does not exist.")
-    os.exit(1)
-
-        
-root = Tk()
-root.geometry("777x575")
-root.minsize(777, 575)
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=200)
-
-root.rowconfigure(0, weight=1)
-
-
-### File Bar
-menubar = Menu(root)
-
-# File
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open", command="")
-filemenu.add_command(label="Save", command=save_notepad)
-filemenu.add_command(label="Close", command="")
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-
-
-# Help
-helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="User Manual", command="")
-helpmenu.add_command(label="Website", command="")
-helpmenu.add_separator()
-helpmenu.add_command(label="About", command="")
-menubar.add_cascade(label="Help", menu=helpmenu)
-
-# Stack our config
-root.config(menu=menubar)
-
-
-### UI Interface layout
-# Frames
-left_frame = Frame(root)
-left_frame.pack(side="left", fill="y")
-
-right_frame = Frame(root)
-right_frame.pack(side="right", fill="both", expand=1)
-
-
-# Category Buttons
-cat_btn_frame = Frame(left_frame)
-cat_btn_frame.pack(side="top")
-
-cat_add = Button(cat_btn_frame, text="Add", command=add_categ)
-cat_add.pack(side="left")
-
-cat_edit = Button(cat_btn_frame, text="Edit", command="")
-cat_edit.pack(side="left")
-
-cat_del = Button(cat_btn_frame, text="Delete", command="")
-cat_del.pack(side="left")
-
-
-# Categories Listview
-cat_list_frame = Frame(left_frame)
-cat_list_frame.pack(side="top")
-
-cat_scrllbr = Scrollbar(cat_list_frame)
-cat_scrllbr.pack(side=RIGHT, fill=Y)
-
-cat_list = Listbox(cat_list_frame, yscrollcommand=cat_scrllbr.set)
-n = 0
-for categ in categories:
-    cat_list.insert(n, categ)
-    cat_list.bind('<Double-1>', categ_dc_event)
-    n += 1
-    
-cat_list.pack(side="top")
-
-
-# File listview
-file_list_frame = Frame(left_frame)
-file_list_frame.pack(side="top")
-
-file_scrllbr = Scrollbar(file_list_frame)
-file_scrllbr.pack(side=RIGHT, fill=Y)
-
-file_list = Listbox(file_list_frame, yscrollcommand=file_scrllbr.set)
-file_list.insert(0, "species.txt")
-file_list.insert(1, "recipes.txt")
-file_list.insert(2, "procedures.txt")
-file_list.insert(3, "notes.txt")
-file_list.pack(side="top")
-
-
-# Notepad Window
-notepad = Text(right_frame)
-notepad.pack(side="right", fill="both", expand=1)
-
-
-
-
-# __main__ loop()
-root.mainloop()
+    def mainloop(self):
+        self.root.mainloop()
---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+
+
+# PyPad - Python Organized ScratchPad for scientific purposes
+#
+# v0.0.1 Beta
+# Date: 8-19-2023
+# Author: Tom Smith (Thomas DOT Briggs DOT Smith AT Gmail DOT com)
+
+
+
+from PyPad import *
+
+
+# Environment variables
+user_data = "userData"
+categ_path = "./" + user_data + "/Categories/"
+
+slctn_path = {'ctgry' : '', 'file' : ''}
+
+
+# Operation Variables
+
+
+
+
+
+
+
+
+
+# Main loop()
+GUI = PyPadGUI()
+GUI.mainloop()
